c26cf568d45b9dc7bb77072b3693c314df8186e2
Kristian Perriu <92625983+21Christian@users.noreply.github.com>
2022-10-22 19:26:06 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+# define the function 
 def fuzzbuzz(alist):
+#     start looping through the list
     for i in alist:
+        # check for the numbers
         if i % 5 == 0 and i % 3 != 0:
             print(i, 'fuzz')
         elif i % 3 == 0 and i % 5 != 0:
@@ -8,7 +11,7 @@ def fuzzbuzz(alist):
             print(i, 'fuzzbuzz')
         else:
             print(i, 'neither')
-
+# populate the list
 my_list = []
 for j in range(1, 101):
     my_list.append(j)
